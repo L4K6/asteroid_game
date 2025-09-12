117a545b60ce78ab68bca72c3e35161a2b543141
L4K6 <je.pavel@seznam.cz>
2025-09-12 20:44:23 +0200

    created screen, made it black

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from constants import *
 import pygame
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while screen.fill("black"):
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
     print("Starting Asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
