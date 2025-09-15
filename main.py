a0a9c8f4f83ad305efea17738baeae23262e1fe6
L4K6 <je.pavel@seznam.cz>
2025-09-15 22:24:11 +0200

    added the ability to shoot and internal attack cooldown

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
+
+
 Player.containers = (updatable, drawable)
 Asteroid.containers = (asteroids, updatable, drawable)
 AsteroidField.containers = updatable
+Shot.containers = (shots, updatable, drawable)
+
 asteroid_field = AsteroidField()
 
 def main():
